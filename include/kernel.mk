641c5b55fc27c28b687367355ec1b116d7869ffe
Dongming Han <handongming@gl-inet.com>
2018-09-10 11:26:06 +0800

    update default kernel vermagic

--- include/kernel.mk
+++ include/kernel.mk
@@ -37,7 +37,7 @@ else
   LINUX_DIR ?= $(KERNEL_BUILD_DIR)/linux-$(LINUX_VERSION)
   LINUX_UAPI_DIR=uapi/
   LINUX_VERMAGIC:=$(strip $(shell cat $(LINUX_DIR)/.vermagic 2>/dev/null))
-  LINUX_VERMAGIC:=$(if $(LINUX_VERMAGIC),$(LINUX_VERMAGIC),11c59ca79402ee01def9dee237272171)
+  LINUX_VERMAGIC:=$(if $(LINUX_VERMAGIC),$(LINUX_VERMAGIC),d4fe45187c77b49cd9dc78ce7d0c9b1d)
 
   LINUX_UNAME_VERSION:=$(if $(word 3,$(subst ., ,$(KERNEL_BASE))),$(KERNEL_BASE),$(KERNEL_BASE).0)
   ifneq ($(findstring -rc,$(LINUX_VERSION)),)
